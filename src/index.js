4395b05e523a926c1f5ab6958184157a0ece7816
tkornilova <kornilova.tat07@gmail.com>
2022-06-23 12:14:34 +0400

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+  const numStr = String(Math.abs(num));
+  let reversedNum = '';
+
+  for (let i=0; i < numStr.length; i++) {
+    reversedNum = `${numStr[i]}${reversedNum}`;
+  }
+
+  return Number(reversedNum);
 }
